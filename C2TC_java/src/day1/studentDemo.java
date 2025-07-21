5d4a2b5f1a1f38598cc45a3f30ceec9e7d1c95b9
DeepakKumarV-2004 <deepakvel2004@gmail.com>
2025-07-21 16:30:40 +0530

    firstencapsulation

---
+++ C2TC_java/src/day1/studentDemo.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class studentDemo {
+	public static void main(String[] args) {
+		student sb = new student();
+		sb.setId(56);
+		sb.setName("Hello");
+		System.out.println(sb.getId());
+		System.out.println(sb.getName());
+		System.out.println();
+		System.out.println(sb.toString());
+			
+	}
+}
