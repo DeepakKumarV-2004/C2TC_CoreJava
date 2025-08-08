72834a16b8b75e20f41e344da52d0912285d7467
DeepakKumarV-2004 <deepakvel2004@gmail.com>
2025-08-08 15:38:33 +0530

    corejava

---
+++ C2TC_java/src/day14/syncronization/Bank.java
@@ -0,0 +1,8 @@
+package day14.syncronization;
+
+public interface Bank {
+	int MINBAL=5000;
+	static final int DAILY_LIMIT=25000;
+	public abstract void deposit(int amt) throws DepositLimitExceedsException;
+	void withdraw(int amt)throws InsufficientBalanceException;
+}
